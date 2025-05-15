4b5a001e991bb70f1ec183a90b06bbb1f082e414
Krunal Patel <krunalpintoo@gmail.com>
2025-05-15 12:43:22 -0400

    dsds

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 23578, hash: 'dbafeb3065f352ebbaa8e702877e48d94c2683b5f1e1c54c533394dd0605fe7e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17152, hash: '262ea6c1e28685f9e677d9360251d218b516d1e2d9fe291bde56c7edcb7f82a9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 23578, hash: '5d29bab6b9ae1109526feb8ca912f978b47eda3b4e04a3905ae04153d3096b04', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17152, hash: '67addf6982c90ede35003b2c3919e15e1c491ff59c384a7cc7cf1cb904bd898e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-36AW6TKX.css': {size: 6979, hash: 'vY6tjD/ce7M', text: () => import('./assets-chunks/styles-36AW6TKX_css.mjs').then(m => m.default)}
   },
 };
