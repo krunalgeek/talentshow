162f6d8367241aeba645efda9c85bb8d1da87b6a
Krunal Patel <krunalpintoo@gmail.com>
2025-05-16 20:24:58 -0400

    dsds

---
+++ docs/angular-app-manifest.mjs
@@ -0,0 +1,14 @@
+
+export default {
+  bootstrap: () => import('./main.server.mjs').then(m => m.default),
+  inlineCriticalCss: true,
+  baseHref: '/talentshow/',
+  locale: undefined,
+  routes: undefined,
+  entryPointToBrowserMapping: {},
+  assets: {
+    'index.csr.html': {size: 23578, hash: '69afbfbd0fd8533040529b368499ff4a5f855ee40e26312768aadcf0e1abc024', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17152, hash: 'e163847e80b31ec2faaa0e44a54baf89d0719ee52715a58f6508a21e2307df88', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-36AW6TKX.css': {size: 6979, hash: 'vY6tjD/ce7M', text: () => import('./assets-chunks/styles-36AW6TKX_css.mjs').then(m => m.default)}
+  },
+};
